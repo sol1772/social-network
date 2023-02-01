7c2f0ca882718009107f46279a49657d2716f3b4
SalavatM <techprosm@yandex.ru>
2023-02-01 17:51:53 +0300

    web lesson14 test

--- dao/src/test/java/com/getjavajob/training/maksyutovs/socialnetwork/dao/AccountDAOTest.java
+++ dao/src/test/java/com/getjavajob/training/maksyutovs/socialnetwork/dao/AccountDAOTest.java
@@ -1,4 +1,326 @@
+package com.getjavajob.training.maksyutovs.socialnetwork.dao;
+
+import com.getjavajob.training.maksyutovs.socialnetwork.domain.Account;
+import org.junit.jupiter.api.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AccountDAOTest {
-  
+
+    private static final String resourceName = "/h2.properties";
+    private static AccountDAO dao;
+    private static Connection con;
+    private static Statement st;
+    private static ResultSet rs;
+
+    @BeforeAll
+    static void connect() {
+        dao = new AccountDAO(resourceName);
+        con = dao.getConnection();
+        try {
+            con.setAutoCommit(false);
+            st = con.createStatement();
+            // creating tables
+            String query = "CREATE TABLE if not exists Account (" +
+                    "    id INT AUTO_INCREMENT," +
+                    "    firstName VARCHAR(30) NOT NULL," +
+                    "    lastName VARCHAR(30) NOT NULL," +
+                    "    middleName VARCHAR(30)," +
+                    "    username VARCHAR(15) NOT NULL," +
+                    "    email VARCHAR(50) NOT NULL UNIQUE," +
+                    "    dateOfBirth DATE NOT NULL," +
+                    "    gender ENUM('M', 'F')," +
+                    "    addInfo VARCHAR(100)," +
+                    "    passwordHash VARCHAR(128)," +
+                    "    registeredAt DATETIME," +
+                    "    PRIMARY KEY(id)" +
+                    "); " +
+                    "CREATE TABLE if not exists Phone (" +
+                    "    id INT AUTO_INCREMENT," +
+                    "    accId INT," +
+                    "    phoneNmr VARCHAR (15) NOT NULL," +
+                    "    phoneType ENUM('PERSONAL', 'WORK')," +
+                    "    PRIMARY KEY(id)," +
+                    "    CONSTRAINT Uq_phones UNIQUE (accId, phoneNmr, phoneType)" +
+                    "); " +
+                    "CREATE TABLE if not exists Address (" +
+                    "    id INT AUTO_INCREMENT," +
+                    "    accId INT," +
+                    "    addr VARCHAR (100) NOT NULL," +
+                    "    addrType ENUM('HOME', 'WORK')," +
+                    "    PRIMARY KEY(id)," +
+                    "    CONSTRAINT Uq_addresses UNIQUE (accId, addr, addrType)" +
+                    "); " +
+                    "CREATE TABLE if not exists Messenger (" +
+                    "    id INT AUTO_INCREMENT," +
+                    "    accId INT," +
+                    "    username VARCHAR (50) NOT NULL," +
+                    "    msngrType ENUM('SKYPE', 'TELEGRAM', 'WHATSAPP', 'ICQ')," +
+                    "    PRIMARY KEY(id)," +
+                    "    CONSTRAINT Uq_messengers UNIQUE (accId, username, msngrType)" +
+                    "); " +
+                    "CREATE TABLE if not exists Friend (" +
+                    "    id INT AUTO_INCREMENT," +
+                    "    accId INT NOT NULL," +
+                    "    friendID INT NOT NULL," +
+                    "    PRIMARY KEY(id)," +
+                    "    CONSTRAINT Uq_friends UNIQUE (accId, friendID)" +
+                    "); " +
+                    "CREATE TABLE if not exists InterestGroup (" +
+                    "    id INT AUTO_INCREMENT," +
+                    "    createdBy INT, " +
+                    "    title VARCHAR(50) NOT NULL UNIQUE, " +
+                    "    metaTitle VARCHAR(100), " +
+                    "    createdAt DATETIME," +
+                    "    PRIMARY KEY(id)" +
+                    "); " +
+                    "CREATE TABLE if not exists Group_member (" +
+                    "    id INT AUTO_INCREMENT," +
+                    "    accId INT NOT NULL," +
+                    "    groupId INT NOT NULL, " +
+                    "    roleType ENUM('ADMIN', 'MODER', 'MEMBER')," +
+                    "    PRIMARY KEY(id)," +
+                    "    CONSTRAINT Uq_members UNIQUE (accId, groupId)" +
+                    ");";
+            st.executeUpdate(query);
+            con.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            if (con != null) {
+                try {
+                    System.err.print("Transaction is being rolled back");
+                    con.rollback();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @AfterAll
+    static void closeConnection() {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (st != null) {
+                st.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void truncateTables() {
+        String query = "SELECT Concat('TRUNCATE TABLE ',table_schema,'.',TABLE_NAME, ';') AS QueryText " +
+                "FROM INFORMATION_SCHEMA.TABLES where table_schema = 'PUBLIC'";
+        try {
+            con.setAutoCommit(false);
+            rs = st.executeQuery(query);
+            List<String> queries = new ArrayList<>();
+            while (rs.next()) {
+                queries.add(rs.getObject("QueryText").toString());
+            }
+            for (String q : queries) {
+                st.executeUpdate(q);
+                con.commit();
+            }
+            if (queries.size() == 0) {
+                System.out.println("No tables to truncate");
+            } else {
+                System.out.println("All tables truncated before inserting new records");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            if (con != null) {
+                try {
+                    System.err.print("Transaction is being rolled back");
+                    con.rollback();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Order(1)
+    @Test
+    void insert() {
+        System.out.println("---------------------------------");
+        System.out.println("Test AccountDAO.insert(Account)");
+        Account account;
+        try {
+            truncateTables();
+            account = new Account("Kamila", "Valieva", "kamila_valieva",
+                    dao.formatter.parse("2006-04-26"), "info@valievakamila.ru");
+            account.setMiddleName("Valerievna");
+            account.setGender(Account.Gender.F);
+            account.setAddInfo("some info");
+            account.setPasswordHash(account.hashPassword("ComplicatedPassword_1"));
+            Account dbAccount = dao.insert("", account);
+            assertNotNull(dbAccount);
+            System.out.println("Created account " + account);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Order(2)
+    @Test
+    void insertContacts() {
+        System.out.println("---------------------------------");
+        System.out.println("Test AccountDAO.insert(Contacts)");
+        Account account;
+        String email = "info@valievakamila.ru";
+        account = dao.select("", "Email", email);
+        assertNotNull(account);
+
+        Account dbAccount;
+        List<Account.Phone> phones = account.getPhones();
+        phones.add(account.new Phone("+79161234567", Account.PhoneType.PERSONAL));
+        phones.add(account.new Phone("+74951234567", Account.PhoneType.WORK));
+        dbAccount = dao.insert("", phones);
+        assertEquals(phones.size(), dbAccount.getPhones().size());
+
+        List<Account.Address> addresses = account.getAddresses();
+        addresses.add(account.new Address("Kazan", Account.AddressType.HOME));
+        addresses.add(account.new Address("Moscow", Account.AddressType.WORK));
+        dbAccount = dao.insert("", addresses);
+        assertEquals(addresses.size(), dbAccount.getAddresses().size());
+
+        List<Account.Messenger> messengers = account.getMessengers();
+        messengers.add(account.new Messenger("kamilavalieva", Account.MessengerType.SKYPE));
+        messengers.add(account.new Messenger("@kamilavalieva26official", Account.MessengerType.TELEGRAM));
+        dbAccount = dao.insert("", messengers);
+        assertEquals(messengers.size(), dbAccount.getMessengers().size());
+    }
+
+    @Order(3)
+    @Test
+    void insertFriends() {
+        System.out.println("---------------------------------");
+        System.out.println("Test AccountDAO.insert(Friends)");
+        Account account;
+        try {
+            String email = "info@valievakamila.ru";
+            account = dao.select("", "Email", email);
+            assertNotNull(account);
+            String email2 = "info@alinazagitova.ru";
+            Account friendAccount = dao.select("", "Email", email2);
+            if (friendAccount == null) {
+                friendAccount = new Account("Alina", "Zagitova", "alina_zagitova",
+                        dao.formatter.parse("2002-05-18"), email2);
+                friendAccount.setGender(Account.Gender.F);
+                dao.insert("", friendAccount);
+                friendAccount = dao.select("", "Email", email2);
+                System.out.println("Created friend account " + friendAccount);
+            }
+            List<Account.Friend> friends = account.getFriends();
+            friends.add(account.new Friend(friendAccount));
+            Account dbAccount = dao.insert("", friends);
+            assertEquals(friends.size(), dbAccount.getFriends().size());
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Order(4)
+    @Test
+    void select() {
+        System.out.println("---------------------------------");
+        System.out.println("Test AccountDAO.select()");
+        String email = "info@valievakamila.ru";
+        Account account = dao.select("", "Email", email);
+        assertNotNull(account);
+        assertEquals(email, account.getEmail());
+    }
+
+    @Order(5)
+    @Test
+    void update() {
+        System.out.println("---------------------------------");
+        System.out.println("Test AccountDAO.update()");
+        String email = "info@valievakamila.ru";
+        Account account = dao.select("", "Email", email);
+        assertNotNull(account);
+        // updating a field
+        String valueToChange = "Some info about Kamila Valieva";
+        Account dbAccount = dao.update("", "addInfo", valueToChange, account);
+        assertEquals(valueToChange, dbAccount.getAddInfo());
+
+        // updating contacts via query
+        String query = "PHONE SET phoneNmr='+79876543210' WHERE ACCID=" + account.getId()
+                + " AND phoneType='personal';";
+        dbAccount = dao.update(query, "", "", account);
+        List<Account.Phone> phones = dbAccount.getPhones();
+        assertEquals("+79876543210", Objects.requireNonNull(phones.stream().filter(phone ->
+                Account.PhoneType.PERSONAL.equals(phone.getPhoneType())).findAny().orElse(null)).getNumber());
+        System.out.println("Updated account " + account);
+    }
+
+    @Order(6)
+    @Test
+    void deleteContacts() {
+        System.out.println("---------------------------------");
+        System.out.println("Test AccountDAO.delete(Contacts)");
+        String email = "info@valievakamila.ru";
+        Account account = dao.select("", "Email", email);
+        assertNotNull(account);
+
+        List<Account.Phone> phones = account.getPhones();
+        phones.clear();
+        phones.add(account.new Phone("+74951234567", Account.PhoneType.WORK));
+        Account dbAccount = dao.delete("", phones);
+        assertEquals(1, dbAccount.getPhones().size());
+        System.out.println("Deleted contact");
+    }
+
+    @Order(7)
+    @Test
+    void deleteFriends() {
+        System.out.println("---------------------------------");
+        System.out.println("Test AccountDAO.delete(Friends)");
+        String email = "info@valievakamila.ru";
+        Account account = dao.select("", "Email", email);
+        assertNotNull(account);
+        String email2 = "info@alinazagitova.ru";
+        Account friend = dao.select("", "Email", email2);
+        assertNotNull(friend);
+
+        List<Account.Friend> friends = account.getFriends();
+        int initialQuantity = friends.size();
+        friends.clear();
+        friends.add(account.new Friend(friend));
+        Account dbAccount = dao.delete("", friends);
+        assertEquals(initialQuantity - 1, dbAccount.getFriends().size());
+        System.out.println("Deleted friend");
+    }
+
+    @Order(8)
+    @Test
+    void delete() {
+        System.out.println("---------------------------------");
+        System.out.println("Test AccountDAO.delete(Account)");
+        String email = "info@valievakamila.ru";
+        Account account = dao.select("", "Email", email);
+        assertNotNull(account);
+
+        Account dbAccount = dao.delete("", account);
+        assertNull(dbAccount);
+        System.out.println("Deleted account " + account);
+    }
+
 }
--- dao/src/test/java/com/getjavajob/training/maksyutovs/socialnetwork/dao/GroupDAOTest.java
+++ dao/src/test/java/com/getjavajob/training/maksyutovs/socialnetwork/dao/GroupDAOTest.java
@@ -1,4 +1,257 @@
+package com.getjavajob.training.maksyutovs.socialnetwork.dao;
+
+import com.getjavajob.training.maksyutovs.socialnetwork.domain.Account;
+import com.getjavajob.training.maksyutovs.socialnetwork.domain.Group;
+import org.junit.jupiter.api.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class GroupDAOTest {
-  
+
+    private static final String resourceName = "/h2.properties";
+    private static GroupDAO dao;
+    private static Connection con;
+    private static Statement st;
+    private static ResultSet rs;
+
+    @BeforeAll
+    static void connect() {
+        dao = new GroupDAO(resourceName);
+        con = dao.getConnection();
+        try {
+            con.setAutoCommit(false);
+            st = con.createStatement();
+            String query = "CREATE TABLE if not exists InterestGroup (" +
+                    "    id INT AUTO_INCREMENT," +
+                    "    createdBy INT, " +
+                    "    title VARCHAR(50) NOT NULL UNIQUE, " +
+                    "    metaTitle VARCHAR(100), " +
+                    "    createdAt DATETIME," +
+                    "    PRIMARY KEY(id)" +
+                    ");" +
+                    "CREATE TABLE if not exists Group_member (" +
+                    "    id INT AUTO_INCREMENT," +
+                    "    accId INT NOT NULL," +
+                    "    groupId INT NOT NULL, " +
+                    "    roleType ENUM('ADMIN', 'MODER', 'MEMBER')," +
+                    "    PRIMARY KEY(id)," +
+                    "    CONSTRAINT Uq_members UNIQUE (accId, groupId)" +
+                    ");";
+            st.executeUpdate(query);
+            con.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            if (con != null) {
+                try {
+                    System.err.print("Transaction is being rolled back");
+                    con.rollback();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @AfterAll
+    static void closeConnection() {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (st != null) {
+                st.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void truncateTables() {
+        String query = "TRUNCATE TABLE INTERESTGROUP";
+        try {
+            con.setAutoCommit(false);
+            st.executeUpdate(query);
+            query = "TRUNCATE TABLE GROUP_MEMBER";
+            st.executeUpdate(query);
+            System.out.println("'InterestGroup' and 'Group_member' tables truncated before adding a new group");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            if (con != null) {
+                try {
+                    System.err.print("Transaction is being rolled back");
+                    con.rollback();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Order(1)
+    @Test
+    void insert() {
+        System.out.println("---------------------------------");
+        System.out.println("Test GroupDAO.insert(Group)");
+        Group group;
+        AccountDAO accountDAO = new AccountDAO(con);
+        try {
+            truncateTables();
+            // registering new group
+            group = new Group("Figure skating");
+            group.setMetaTitle("Figure skating fans group");
+            // admin of the group
+            String email = "info@alinazagitova.ru";
+            Account account = accountDAO.select("", "Email", email);
+            if (account == null) {
+                account = new Account("Alina", "Zagitova", "alina_zagitova",
+                        dao.formatter.parse("2002-05-18"), email);
+                account.setGender(Account.Gender.F);
+                accountDAO.insert("", account);
+                account = accountDAO.select("", "Email", email);
+            }
+            group.setCreatedBy(account.getId());
+            Group dbGroup = dao.insert("", group);
+            assertNotNull(dbGroup);
+            System.out.println("Created group " + group);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Order(2)
+    @Test
+    void insertMembers() {
+        System.out.println("---------------------------------");
+        System.out.println("Test GroupDAO.insert(Members)");
+        Group group;
+        AccountDAO accountDAO = new AccountDAO(con);
+        try {
+            group = dao.select("", "Title", "Figure skating");
+            if (group == null) {
+                // registering new group
+                group = new Group("Figure skating");
+                group.setMetaTitle("Figure skating fans group");
+                // admin of the group
+                String email = "info@alinazagitova.ru";
+                Account account = accountDAO.select("", "Email", email);
+                if (account == null) {
+                    account = new Account("Alina", "Zagitova", "alina_zagitova",
+                            dao.formatter.parse("2002-05-18"), email);
+                    account.setGender(Account.Gender.F);
+                    accountDAO.insert("", account);
+                    account = accountDAO.select("", "Email", email);
+                }
+                group.setCreatedBy(account.getId());
+                dao.insert("", group);
+                System.out.println("Created group " + group);
+            }
+
+            List<Group.GroupMember> members = group.getMembers();
+            Account account1 = accountDAO.select("", "id", group.getCreatedBy());
+            members.add(group.new GroupMember(account1, Group.Role.ADMIN));
+            // moderator of the group
+            String email2 = "dari@tat.ru";
+            Account account2 = accountDAO.select("", "Email", email2);
+            if (account2 == null) {
+                account2 = new Account("Darina", "Sabitova", "darisabitova",
+                        dao.formatter.parse("2007-01-12"), email2);
+                account2.setGender(Account.Gender.F);
+                accountDAO.insert("", account2);
+                account2 = accountDAO.select("", "Email", email2);
+            }
+            members.add(group.new GroupMember(account2, Group.Role.MEMBER));
+            Group dbGroup = dao.insert("", members);
+            assertEquals(members.size(), dbGroup.getMembers().size());
+            System.out.println("Added members: " + account1 + " and " + account2);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Order(3)
+    @Test
+    void select() {
+        System.out.println("---------------------------------");
+        System.out.println("Test GroupDAO.select()");
+        String title = "Figure skating";
+        Group group = dao.select("", "Title", title);
+        assertNotNull(group);
+        assertEquals(title, group.getTitle());
+    }
+
+    @Order(4)
+    @Test
+    void update() {
+        System.out.println("---------------------------------");
+        System.out.println("Test GroupDAO.update()");
+        String title = "Figure skating";
+        Group group = dao.select("", "Title", title);
+        assertNotNull(group);
+        // updating a field
+        String valueToChange = "Figure skating fans group 2023";
+        Group dbGroup = dao.update("", "metaTitle", valueToChange, group);
+        assertEquals(valueToChange, dbGroup.getMetaTitle());
+
+        // updating members via query
+        AccountDAO accountDAO = new AccountDAO(con);
+        String email = "dari@tat.ru";
+        Account account = accountDAO.select("", "Email", email);
+        assertNotNull(account);
+        String query = "Group_member SET roleType='moder' WHERE groupId=" + group.getId()
+                + " AND accId=" + account.getId() + ";";
+        dbGroup = dao.update(query, "", "", group);
+        List<Group.GroupMember> members = dbGroup.getMembers();
+        assertEquals(Group.Role.MODER, Objects.requireNonNull(members.stream().filter
+                (member -> member.getAccount().getId() == account.getId()).findAny().orElse(null)).getRole());
+        System.out.println("Updated group " + group);
+    }
+
+    @Order(5)
+    @Test
+    void deleteMembers() {
+        System.out.println("---------------------------------");
+        System.out.println("Test GroupDAO.delete(Members)");
+        String title = "Figure skating";
+        Group group = dao.select("", "Title", title);
+        assertNotNull(group);
+
+        List<Group.GroupMember> members = group.getMembers();
+        int initialQuantity = members.size();
+        members.clear();
+        AccountDAO accountDAO = new AccountDAO(con);
+        String email = "dari@tat.ru";
+        Account account = accountDAO.select("", "Email", email);
+        assertNotNull(account);
+        members.add(group.new GroupMember(account, Group.Role.MODER));
+        Group dbGroup = dao.delete("", members);
+        assertEquals(initialQuantity - 1, dbGroup.getMembers().size());
+        System.out.println("Deleted member " + account);
+    }
+
+    @Order(6)
+    @Test
+    void delete() {
+        System.out.println("---------------------------------");
+        System.out.println("Test GroupDAO.delete()");
+        String title = "Figure skating";
+        Group group = dao.select("", "Title", title);
+        assertNotNull(group);
+
+        Group dbGroup = dao.delete("", group);
+        assertNull(dbGroup);
+        System.out.println("Deleted group " + group);
+    }
+
 }
