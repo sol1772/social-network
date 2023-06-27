a1e19b40413fc29e54d205f4b44b16d3721d61d3
SalavatM <techprosm@yandex.ru>
2023-06-27 21:01:17 +0300

    web lesson21 service test

--- service/src/test/java/com/getjavajob/training/maksyutovs/socialnetwork/service/AccountServiceTest.java
+++ service/src/test/java/com/getjavajob/training/maksyutovs/socialnetwork/service/AccountServiceTest.java
@@ -1,11 +1,22 @@
 package com.getjavajob.training.maksyutovs.socialnetwork.service;
 
 import com.getjavajob.training.maksyutovs.socialnetwork.dao.AccountDao;
+import com.getjavajob.training.maksyutovs.socialnetwork.dao.Utils;
+import com.getjavajob.training.maksyutovs.socialnetwork.domain.Account;
+import com.getjavajob.training.maksyutovs.socialnetwork.domain.Friend;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
@@ -16,123 +27,109 @@ class AccountServiceTest {
 
     @BeforeEach
     void initService() {
-        accountService = new AccountService();
-        accountService.setDao(mockDao);
+        accountService = new AccountService(mockDao);
     }
 
     @Test
     void registerAccount() {
-//        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
-//                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
-//        when(mockDao.insert("", account)).thenReturn(account);
-//        Account savedAccount = accountService.registerAccount(account);
-//        assertNotNull(savedAccount);
-//        assertEquals(account, savedAccount);
+        Account account = new Account();
+        when(mockDao.insert(any(Account.class))).then(returnsFirstArg());
+        Account savedAccount = accountService.registerAccount(account);
+        assertNotNull(savedAccount);
     }
 
     @Test
     void getAccountByEmail() {
-//        String email = "info@valievakamila.ru";
-//        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
-//                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), email);
-//        when(mockDao.select("", "Email", email)).thenReturn(account);
-//        assertEquals(account, accountService.getAccountByEmail(email));
+        String email = "info@valievakamila.ru";
+        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
+                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), email);
+        when(mockDao.select(eq("email"), anyString())).thenReturn(account);
+        Account savedAccount = accountService.getAccountByEmail(account.getEmail());
+        assertEquals(email, savedAccount.getEmail());
     }
 
     @Test
     void editAccount() {
-//        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
-//                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
-//        account.setAddInfo("Some info");
-//        lenient().doThrow(NoSuchElementException.class).when(mockDao).update(
-//                anyString(), anyString(), anyString(), any(Account.class));
-//        Optional<Account> savedAccount = accountService.editAccount(account, "addInfo", "New info");
-//        assertThrows(NoSuchElementException.class, savedAccount::get);
+        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
+                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
+        account.setAddInfo("Some info");
+        when(mockDao.selectByEmail(anyString())).thenReturn(true);
+        when(mockDao.update(anyString(), anyString(), any(Account.class))).thenReturn(account);
+        Account savedAccount = accountService.editAccount(account, "addInfo", "New info");
+        assertEquals(account.getAddInfo(), savedAccount.getAddInfo());
     }
 
     @Test
     void deleteAccount() {
-//        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
-//                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
-//        lenient().doThrow(NoSuchElementException.class).when(mockDao).delete("", account);
-//        Optional<Account> deletedAccount = accountService.deleteAccount(account);
-//        assertThrows(NoSuchElementException.class, deletedAccount::get);
+        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
+                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
+        when(mockDao.selectByEmail(anyString())).thenReturn(true);
+        when(mockDao.delete(any(Account.class))).thenReturn(null);
+        Account deletedAccount = accountService.deleteAccount(account);
+        assertNull(deletedAccount);
     }
 
     @Test
     void addFriend() {
-//        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
-//                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
-//        Account friend = new Account("Alina", "Zagitova", "alina_zagitova",
-//                LocalDate.parse("2002-05-18", Utils.DATE_FORMATTER), "info@alinazagitova.ru");
-//        List<Friend> friends = account.getFriends();
-//        friends.add(new Friend(account, friend));
-//
-//        lenient().when(mockDao.insert("", friends)).thenReturn(account);
-//        Optional<Account> savedAccount = accountService.addFriend(account, friend);
-//        assertThrows(NoSuchElementException.class, savedAccount::get);
+        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
+                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
+        Account friend = new Account("Alina", "Zagitova", "alina_zagitova",
+                LocalDate.parse("2002-05-18", Utils.DATE_FORMATTER), "info@alinazagitova.ru");
+        List<Friend> friends = account.getFriends();
+        friends.add(new Friend(account, friend));
+
+        when(mockDao.select(eq("email"), anyString())).thenReturn(account);
+        when(mockDao.insert(friends)).thenReturn(account);
+        Account savedAccount = accountService.addFriend(account, friend);
+        assertEquals(friends.size(), savedAccount.getFriends().size());
     }
 
     @Test
     void deleteFriend() {
-//        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
-//                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
-//        Account friend = new Account("Alina", "Zagitova", "alina_zagitova",
-//                LocalDate.parse("2002-05-18", Utils.DATE_FORMATTER), "info@alinazagitova.ru");
-//        List<Friend> friends = account.getFriends();
-//        friends.add(new Friend(account, friend));
-//
-//        lenient().when(mockDao.delete("", friends)).thenReturn(account);
-//        Optional<Account> savedAccount = accountService.deleteFriend(account, friend);
-//        assertThrows(NoSuchElementException.class, savedAccount::get);
+        Account account = new Account();
+        Account friend = new Account();
+        when(mockDao.select("email", account.getEmail())).thenReturn(account);
+        when(mockDao.delete(account.getFriends())).thenReturn(account);
+        assertNotNull(accountService.deleteFriend(account, friend));
     }
 
     @Test
     void getFriends() {
-//        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
-//                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
-//        Account friend = new Account("Alina", "Zagitova", "alina_zagitova",
-//                LocalDate.parse("2002-05-18", Utils.DATE_FORMATTER), "info@alinazagitova.ru");
-//        List<Friend> friends = account.getFriends();
-//        friends.add(new Friend(account, friend));
-//
-//        lenient().when(mockDao.insert("", friends)).thenReturn(account);
-//        when(mockDao.select(anyString(), anyString(), anyString())).thenReturn(account);
-//        assertEquals(1, accountService.getFriends(account).size());
+        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
+                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
+        Account friend = new Account("Alina", "Zagitova", "alina_zagitova",
+                LocalDate.parse("2002-05-18", Utils.DATE_FORMATTER), "info@alinazagitova.ru");
+        List<Friend> friends = account.getFriends();
+        friends.add(new Friend(account, friend));
+
+        when(mockDao.select(anyString(), anyString())).thenReturn(account);
+        assertEquals(1, accountService.getFriends(account).size());
     }
 
     @Test
     void passwordIsValid() {
-//        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
-//                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
-//        String password = "ComplicatedPassword_1";
-//        account.setPasswordHash(account.hashPassword(password));
-//        lenient().doThrow(NoSuchElementException.class).when(mockDao).update(
-//                anyString(), anyString(), anyString(), any(Account.class));
-//        Optional<Account> savedAccount = accountService.editAccount(account, "passwordHash", password);
-//        if (savedAccount.isEmpty()) {
-//            assertThrows(NoSuchElementException.class, savedAccount::get);
-//        } else {
-//            assertTrue(accountService.passwordIsValid(password, savedAccount.get()));
-//        }
+        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
+                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), "info@valievakamila.ru");
+        String password = "ComplicatedPassword_1";
+        account.setPasswordHash(account.hashPassword(password));
+
+        when(mockDao.selectByEmail(anyString())).thenReturn(true);
+        when(mockDao.update(anyString(), anyString(), any(Account.class))).thenReturn(account);
+        Account savedAccount = accountService.editAccount(account, "passwordHash", password);
+        assertTrue(accountService.passwordIsValid(password, savedAccount));
     }
 
     @Test
     void changePassword() {
-//        String email = "info@valievakamila.ru";
-//        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
-//                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), email);
-//        when(mockDao.select("", "Email", email)).thenReturn(account);
-//        Account dbAccount = accountService.getAccountByEmail(email);
-//        String oldPassword = dbAccount.getPasswordHash();
-//        String newPassword = "ComplicatedPassword_1";
-//        account.setPasswordHash(account.hashPassword(newPassword));
-//        Optional<Account> savedAccount = accountService.editAccount(account, "passwordHash", newPassword);
-//        if (savedAccount.isEmpty()) {
-//            assertThrows(NoSuchElementException.class, savedAccount::get);
-//        } else {
-//            assertTrue(accountService.changePassword(oldPassword, newPassword, savedAccount.get()));
-//        }
+        String email = "info@valievakamila.ru";
+        Account account = new Account("Kamila", "Valieva", "kamila_valieva",
+                LocalDate.parse("2006-04-26", Utils.DATE_FORMATTER), email);
+        String oldPassword = "ComplicatedPassword_1";
+        String newPassword = "ComplicatedPassword_2";
+
+        when(mockDao.selectByEmail(anyString())).thenReturn(true);
+        when(mockDao.update(anyString(), anyString(), any(Account.class))).thenReturn(account);
+        assertTrue(accountService.changePassword(oldPassword, newPassword, account));
     }
 
 }
