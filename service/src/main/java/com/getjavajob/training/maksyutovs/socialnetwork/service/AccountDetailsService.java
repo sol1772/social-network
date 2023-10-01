b2a55b821c628d2100032344a98780b70aa76e80
SalavatM <techprosm@yandex.ru>
2023-10-01 12:57:07 +0300

    web lesson26 fixes

--- service/src/main/java/com/getjavajob/training/maksyutovs/socialnetwork/service/AccountDetailsService.java
+++ service/src/main/java/com/getjavajob/training/maksyutovs/socialnetwork/service/AccountDetailsService.java
@@ -29,4 +29,5 @@ public class AccountDetailsService implements UserDetailsService {
         }
         return new AccountDetails(account);
     }
+
 }
--- web/src/main/java/com/getjavajob/training/maksyutovs/socialnetwork/web/config/SecurityWebApplicationInitializer.java
+++ web/src/main/java/com/getjavajob/training/maksyutovs/socialnetwork/web/config/SecurityWebApplicationInitializer.java
@@ -9,5 +9,4 @@ public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplic
         return true;
     }
 
-
 }
